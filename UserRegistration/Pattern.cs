d6d10a0e26d02c3e4d3ada41d6bf8a735f789e9f
SoumyaD27 <soumyad233@gmail.com>
2022-12-19 17:11:14 +0530

    [All use case of User Registration from 1 to 8.]

---
+++ UserRegistration/Pattern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Pattern
+    {
+        public static string firstName = "^[A-Z][a-z]{5}$";
+        public bool validateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, firstName);
+        }
+        public static string lastName = "^[A-Z]$";
+        public bool validatelasttName(string LastName)
+        {
+            return Regex.IsMatch(LastName, lastName);
+        }
+        public static string EmailId = "^[a-z]{7}[0-9]{3}[@][a-z]{5}[.][a-z]{3}$";
+        public bool validateEmailAddres(string emailId)
+        {
+            return Regex.IsMatch(emailId, EmailId);
+        }
+        public static string phoneNumber = "^[+][0-9]{2}[0-9]{10}$";
+        public bool validatePhoneNumber(string PhoneNumber)
+        {
+            return Regex.IsMatch(PhoneNumber, phoneNumber);
+        }
+        public static string password = "^[A-Z][a-z]{8}[0-9][@]$";
+        public bool validatePassword(string Password)
+        {
+            return Regex.IsMatch(Password, password);
+        }
+    }
+}
---
+++ UserRegistration/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {    
+            //All use case of user registrartion are in it from 1 to 8.
+            Pattern pattern = new Pattern();
+            Console.WriteLine(pattern.validateFirstName("Soumya"));
+            Console.WriteLine(pattern.validatelasttName("D"));
+            Console.WriteLine(pattern.validateEmailAddres("abcdefg589@gmail.com"));
+            Console.WriteLine(pattern.validatePhoneNumber("+917857690564"));
+            Console.WriteLine(pattern.validatePassword("Inkjkgthj7@"));
+        }
+    }
+}
