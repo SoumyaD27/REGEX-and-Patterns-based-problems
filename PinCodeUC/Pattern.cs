d937de3aa6d43d50c58feb81feacdb7c8612d9a8
SoumyaD27 <soumyad233@gmail.com>
2022-12-19 14:04:50 +0530

    [Validating PIN Code with regex pattern.]

---
+++ PinCodeUC/Pattern.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PinCodeUC
+{
+    public class Pattern
+    {
+        public static string ZIPCODE = "^[0-9]{6}$" ;
+        public bool Validate_ZipCode(string ZipCode)
+        {
+            return Regex.IsMatch(ZipCode, ZIPCODE);
+        }
+        public static string ZIPCODE1 = "^[0-9]{3} [0-9]{3}$";
+        public bool ValidateZipCode(string zipCode)
+        {
+            return Regex.IsMatch(zipCode, ZIPCODE1);
+        }
+    }
+}
---
+++ PinCodeUC/Program.cs
@@ -0,0 +1,42 @@
+﻿using  System;
+namespace PinCodeUC
+{
+    class Program
+    {               //UseCase 1, 2, 3, 4 can be performed here. 
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            { 
+                case 1:
+                Console.WriteLine("Enter the ZipCode");                //Try with 400088, A400088, 400088B
+                string ZipCode = Console.ReadLine();
+                bool zip = pattern.Validate_ZipCode(ZipCode);
+                if (zip)
+                {
+                    Console.WriteLine("Correct, its a match.");
+                }
+                else
+                {
+                    Console.WriteLine("Not matching");
+                }
+                break;
+            case 2:
+                Console.WriteLine("Enter the ZipCode");                //Try with 400 088
+                string zipCode = Console.ReadLine();
+                bool zip1 = pattern.ValidateZipCode(zipCode);
+                if (zip1)
+                {
+                    Console.WriteLine("Correct, its a match.");
+                }
+                else
+                {
+                    Console.WriteLine("Not matching");
+                }
+                break;
+            }
+        }
+    }
+}
