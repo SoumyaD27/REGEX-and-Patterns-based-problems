0bd28eecbe11987f9c35b5ac5cc727a91b361fa1
SoumyaD27 <soumyad233@gmail.com>
2022-12-19 16:23:05 +0530

    [Validate Email address with a regex.]

---
+++ EmailUC/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailUC
+{
+    public class Pattern
+    {
+        public static string Email = "^[a-z]{3}[.][a-z]{3}[@][a-z]{10}[.][a-z]{2}[.][a-z]{2}$";
+        public bool Validate_ZipCode(string email)
+        {
+            return Regex.IsMatch(email, Email);
+        }
+    }
+}
---
+++ EmailUC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EmailUC
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Enter the Email ID: ");        //Try with abc.xyz@bridgelabz.co.in
+            string email = Console.ReadLine();
+            bool emailId = pattern.Validate_ZipCode(email);
+            if(emailId)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Not Matching");
+            }
+        }
+    }
+}
